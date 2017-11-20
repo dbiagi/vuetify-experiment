3131b68bd217ab9ead358fbcc5ee62fb23cd4fe5
Diego de Biagi <diegobiagiviana@gmail.com>
2017-11-19 23:46:26 -0200

    Added test cases for category repository

---
+++ test/unit/specs/api/category.spec.js
@@ -0,0 +1,24 @@
+import {CategoryRepository} from '@/api/category'
+
+describe('CategoryRepository.findAll()', () => {
+    it('should return a Promise', () => {
+        let promise = CategoryRepository.findAll()
+        assert.typeOf(promise, 'Promise')
+    })
+
+    it('the Promise should resolve to an array', () => {
+        return CategoryRepository.findAll()
+            .then(data => assert.typeOf(data, 'array'))
+    })
+
+    it('the Promise should resolve to an array of objects containing requried fields', () => {
+        return CategoryRepository.findAll()
+            .then(data => {
+                let category = data.pop()
+
+                let expectedKeys = ['id', 'slug', 'title', 'img']
+
+                assert.hasAllKeys(category, expectedKeys)
+            })
+    })
+})
